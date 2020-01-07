4ad77a564e419db83a3c1f17f2ff10966d65ff8d
jon <jon.hallam@managed-desktop.com>
2020-01-07 15:00:26 +0000

    initial

---
+++ RunIt/PostVagrantUpdatesToServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostVagrantUpdatesToServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //URLtoFlow, username, password, state
+        {
+
+            try
+            {
+                if (args.Length < 1)
+                {
+                    Console.WriteLine("URLtoFlow, username, password, state");
+                }
+                else
+                {
+
+                    var URLtoFlow = args.ElementAt(0);
+                    var username = args.ElementAt(1);
+                    var password = args.ElementAt(2);
+                    var state = args.ElementAt(3);
+                    
+                    var hostname = System.Environment.MachineName;
+
+                    var client = new RestClient(URLtoFlow);
+
+                    var request = new RestRequest(Method.POST);
+                    request.AddHeader("Host", client.BaseUrl.Host);
+                    request.AddHeader("Content-Type", "application/json");
+                    request.AddParameter("undefined", "{  \"userid\": \"" + username + "\",   \"password\": \"" + password + "\", \"outputtype\": \"Json\", \"MovingToState\": \"" + state + "\", \"Hostname\" : \"" + hostname + "\" }", ParameterType.RequestBody);
+                    IRestResponse response = client.Execute(request);
+                }
+            }
+            catch (Exception ex2)
+            {
+                Console.WriteLine(ex2.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ RunIt/RunIt/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Set a time-out value.
+         
+            //Get path to system folder.
+            
+            ProcessStartInfo pInfo = new ProcessStartInfo();
+
+           
+            //Set file name to open.
+            pInfo.FileName = args.ElementAt(0);
+            pInfo.Arguments = args.ElementAt(1);
+            //Start the process.
+            Process p = Process.Start(pInfo);
+            //Wait for window to finish loading.
+            //p.WaitForInputIdle();
+            //Wait for the process to exit or time out.
+          
+            while (p.HasExited == false)
+            {
+                Console.WriteLine(DateTime.Now.Second);
+                System.Threading.Thread.Sleep(1000);
+            }
+            //Check to see if the process is still running.
+          
+
+        }
+    }
+}
